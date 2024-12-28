05be262e045046c9213b90af4f6ce4cab7960421
Žan Mencigar <mencigarzan@gmail.com>
2024-12-28 21:59:07 +0100

    Removed create_user test

--- users/tests.py
+++ users/tests.py
@@ -1,30 +1,3 @@
-import django
-import os
+from django.test import TestCase
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
-django.setup() 
-
-import pytest
-from rest_framework import status
-from rest_framework.test import APITestCase, APIClient
-
-
-class UserViewSetTestCase(APITestCase):
-    @pytest.mark.django_db
-    def test_create_user(self):
-        client = APIClient()
-        user_data = {
-            "username": "testuser",
-            "password": "password123",
-            "name": "Test",
-            "surname": "User",
-        }
-
-        response = client.post('/api/users/', user_data, format='json')
-
-        assert response.status_code == status.HTTP_201_CREATED
-
-        assert response.data['username'] == user_data['username']
-        assert response.data['name'] == user_data['name']
-        assert response.data['surname'] == user_data['surname']
-        assert 'password' not in response.data
+# Create your tests here.
